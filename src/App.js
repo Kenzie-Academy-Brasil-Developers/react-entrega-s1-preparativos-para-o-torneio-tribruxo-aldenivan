42c381570cd5304c90e4621a3d51aaa936d792c0
Aldenivan Jr <aldenivan1207@gmail.com>
2021-09-14 15:10:07 -0300

    Creating the structure of the first page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import FirstPage from "./components/FisrtPage";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <FirstPage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FisrtPage/index.js
@@ -0,0 +1,11 @@
+const FirstPage = () => {
+  return (
+    <>
+      <h1>Torneio Tribruxo</h1>
+      <p>Clique no botão para encontrar os feiticeiros!</p>
+      <button>Começar!</button>
+    </>
+  );
+};
+
+export default FirstPage;
