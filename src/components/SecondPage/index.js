7ad607c717fb3bbfeecbda1e02165a678ddf2455
Aldenivan Jr <aldenivan1207@gmail.com>
2021-09-16 14:12:57 -0300

    Creating the second page

---
+++ src/components/SecondPage/index.js
@@ -0,0 +1,14 @@
+import Students from "../Students";
+import ChooseStudents from "../ChooseStudents";
+
+const SecondPage = () => {
+  return (
+    <>
+      <Students />
+      <ChooseStudents />
+      <button>Tente novamente</button>
+    </>
+  );
+};
+
+export default SecondPage;
