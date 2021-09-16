5c3d7f2657612b966dcf25efcce380438c90c6c0
Aldenivan Jr <aldenivan1207@gmail.com>
2021-09-16 14:11:52 -0300

    Creating the results of the chooses

--- src/components/ChooseStudents/index.js
+++ src/components/ChooseStudents/index.js
@@ -1,1 +1,17 @@
+import "./styles.css";
 
+const ChooseStudents = ({ witches }) => {
+  return (
+    <div className="container">
+      {witches?.map((item, index) => (
+        <div key={index}>
+          <img src={item.image} />
+          <h3 className="name">{item.name}</h3>
+          <h3 className="house">{item.house}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ChooseStudents;
