a132cb4ae6db561b7cb9a76d468254da66cb8047
Aldenivan Jr <aldenivan1207@gmail.com>
2021-09-16 23:29:15 -0300

    Styling the cards with the wizards

--- src/components/ChooseStudents/index.js
+++ src/components/ChooseStudents/index.js
@@ -5,7 +5,20 @@ const ChooseStudents = ({ witches }) => {
     <div className="container">
       {witches?.map((item, index) => (
         <div key={index}>
-          <img src={item.image} />
+          <img
+            src={item.image}
+            className="photo"
+            style={{
+              borderColor:
+                item.house === "Gryffindor"
+                  ? "red"
+                  : item.house === "Hufflepuff"
+                  ? "yellow"
+                  : item.house === "Ravenclaw"
+                  ? "blue"
+                  : "green",
+            }}
+          />
           <h3 className="name">{item.name}</h3>
           <h3 className="house">{item.house}</h3>
         </div>
